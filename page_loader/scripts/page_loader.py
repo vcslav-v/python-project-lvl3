f620079edbf1bd9a6948750f49077b81fc7b82e0
vcslav-v <vcslav.v@gmail.com>
2021-02-01 19:21:44 +0300

    add script

---
+++ page_loader/scripts/page_loader.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""Page-loader script."""
+
+import argparse
+from typing import Tuple
+from page_loader import loader
+import os
+
+DESCRIPTION = 'Dowload page'
+HELP_STRING = 'Path to download'
+
+
+def main():
+    """Page-loader script."""
+    url, output_path = get_arguments()
+    try:
+        print(loader.download(url, output_path))
+    except Exception as e:
+        print(e)
+
+
+def get_arguments() -> Tuple[str, str]:
+    """Take the command-line arguments.
+    url, output
+
+    Returns:
+        (url, output_path)
+    """
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser.add_argument('url', type=str)
+    parser.add_argument(
+        '-o', '--output',
+        help=HELP_STRING,
+        type=str,
+        default=os.getcwd()
+    )
+    args = parser.parse_args()
+    return (str(args.url), str(args.output))
+
+
+if __name__ == "__main__":
+    main()
