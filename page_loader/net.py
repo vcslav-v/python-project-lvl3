3de65324069478736f9546c088621e9528c0e09d
vcslav-v <vcslav.v@gmail.com>
2021-02-09 00:01:56 +0300

    improved error outputs

--- page_loader/net.py
+++ page_loader/net.py
@@ -1,8 +1,8 @@
 import requests
 from page_loader.logger import logger
 
-ERROR_RESPONSE_STATUS = 'response status {status}'
-ERROR_CONTENT_TYPE = 'It is not possible to process this type of content'
+ERROR_RESPONSE_STATUS = 'Response code is {status} 200. URL is {url}'
+ERROR_CONTENT_TYPE = 'In response is not html document. URL is {url}'
 
 
 def get_response_content(url: str, is_html=True) -> bytes:
@@ -11,22 +11,24 @@ def get_response_content(url: str, is_html=True) -> bytes:
         with requests.Session() as session:
             response = session.get(url)
     except Exception as e:
-        logger.exception('Net error. URL is {url}'.format(url=url))
+        logger.error('{ex}: URL is {url}'.format(
+            ex=type(e).__name__, url=url)
+        )
         raise e
 
     if response.status_code != 200:
-        logger.error('Response code is not 200. URL is {url}'.format(url=url))
+        logger.error(
+            ERROR_RESPONSE_STATUS.format(url=url, status=response.status_code)
+        )
         raise ConnectionError(
-            ERROR_RESPONSE_STATUS.format(status=response.status_code)
+            ERROR_RESPONSE_STATUS.format(url=url, status=response.status_code)
         )
 
     if is_html:
         content_type = response.headers['Content-Type'].split(';')[0].strip()
 
         if content_type != 'text/html':
-            logger.error(
-                'Response is not html document. URL is {url}'.format(url=url)
-            )
-            raise ValueError(ERROR_CONTENT_TYPE)
+            logger.error(ERROR_CONTENT_TYPE.format(url=url))
+            raise ValueError(ERROR_CONTENT_TYPE.format(url=url))
 
     return response.content
