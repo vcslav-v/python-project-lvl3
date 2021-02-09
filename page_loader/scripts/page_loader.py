335f87e68c34e026fbfcfc0624ab0a0af209651a
vcslav-v <vcslav.v@gmail.com>
2021-02-09 18:27:45 +0300

    dbg

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -16,7 +16,8 @@ def main():
     url, output_path = get_arguments()
     try:
         print(download(url, output_path))
-    except Exception:
+    except Exception as e:
+        print(e)
         sys.exit(1)
     sys.exit(0)
 
