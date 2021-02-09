0a46cb4e15e214ed2d05a5aa377034facbbc66cb
vcslav-v <vcslav.v@gmail.com>
2021-02-09 18:06:25 +0300

    dbg

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -6,55 +6,17 @@ from typing import Tuple
 from page_loader import download
 import os
 import sys
-import logging
-import logging.config
 
 DESCRIPTION = 'Dowload page'
 HELP_STRING = 'Path to download'
 
 
-LOGGING_CONFIG = {
-    'version': 1,
-    'formatters': {
-        'standart': {
-            'format': '%(asctime)s - %(levelname)s: %(message)s'
-        },
-        'error': {
-            'format': '%(levelname)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'std_handler': {
-            'class': 'logging.StreamHandler',
-            'level': 'DEBUG',
-            'stream': 'ext://sys.stdout',
-            'formatter': 'standart',
-        },
-        'error_handler': {
-            'class': 'logging.StreamHandler',
-            'level': 'ERROR',
-            'stream': 'ext://sys.stderr',
-            'formatter': 'error',
-        },
-    },
-    'loggers': {
-        'script': {
-            'handlers': ['std_handler', 'error_handler'],
-            'level': 'DEBUG',
-        }
-    },
-}
-
-
 def main():
     """Page-loader script."""
-    logging.config.dictConfig(LOGGING_CONFIG)
-    logger = logging.getLogger('script')
     url, output_path = get_arguments()
     try:
         print(download(url, output_path))
-    except Exception as e:
-        logger.debug(e)
+    except Exception:
         sys.exit(1)
     sys.exit(0)
 
