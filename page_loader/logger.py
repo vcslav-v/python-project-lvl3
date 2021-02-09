0d255c6cb659e3c399c96194c406b181bbd98973
vcslav-v <vcslav.v@gmail.com>
2021-02-09 11:55:04 +0300

    add logger dictConfig

--- page_loader/logger.py
+++ page_loader/logger.py
@@ -1,5 +1,40 @@
 import logging
 import logging.config
 
-logging.config.fileConfig('logger.cfg')
+LOGGING_CONFIG = {
+    'version': 1,
+    'formatters': {
+        'standart': {
+            'format': '%(asctime)s - %(levelname)s: %(message)s'
+        },
+        'error': {
+            'format': '%(levelname)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'file_handler': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'level': 'DEBUG',
+            'filename': 'page_loader.log',
+            'mode': 'a',
+            'maxBytes': 10240,
+            'backupCount': 0,
+            'formatter': 'standart',
+        },
+        'error_handler': {
+            'class': 'logging.StreamHandler',
+            'level': 'ERROR',
+            'stream': 'ext://sys.stderr',
+            'formatter': 'error',
+        },
+    },
+    'loggers': {
+        'root': {
+            'handlers': ['file_handler', 'error_handler'],
+            'level': 'DEBUG',
+        }
+    },
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger("root")
